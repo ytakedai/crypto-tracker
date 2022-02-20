42f1ce20273cb4d207ad2341ea71a33119e49f8c
ytakedai <yutotakedai@gmail.com>
2022-02-20 01:42:21 -0800

    Add functionality to read Coinbase and Coinbase Pro files and add calculation for short/long term capital gains

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-README
+# README
 
 ### Steps
-1. Fill out trade data in trades.csv
+1. For Coinbase or Coinbase Pro, generate reports (down below) and paste them in folder `reports`.
+1. For manual entry, fill out trade data in a csv file (or multiple) in folder `reports`, with the first row being `Timestamp,Transaction Type,Asset,Size,Price,Fees`
 1. Set value for `YEAR` var in `runner.py`
 1. Set `FILTER_YEAR` to `True`/`False` depending on if you want a detailed report on a specific year or overall
 1. Run script using `python3 runner.py`
@@ -10,3 +11,15 @@ README
 - pandas
   1. Install [pip](https://www.geeksforgeeks.org/how-to-install-pip-in-macos/) if you haven't already
   1. Install pandas: `pip install pandas`
+
+## Report Generation
+
+### Coinbase
+1. Proceed to https://www.coinbase.com/reports and click on `Generate Report`.
+1. Click on `Generate Report` for CSV report.
+
+### Coinbase Pro
+1. Proceed to https://pro.coinbase.com/profile/statements and click on `Generate > Fills`
+1. Move `Start Date` to earliest date of your first trade or earlier
+1. Change format to `CSV`
+1. Click on `Generate Report`
--- runner.py
+++ runner.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import json
+import os
 
-YEAR = 2021
+YEAR = 2022
 FILTER_YEAR = True
 
 crypto_prices = None
@@ -23,9 +24,63 @@ days_in_month = {
     12:31
 }
 
+def load_reports():
+    global trades
+    trades = pd.DataFrame(columns = ['Timestamp' ,'Transaction Type', 'Asset', 'Size', 'Price', 'Fees'])
+    
+    for file_name in os.listdir('./reports/'):
+        if file_name.endswith('.csv'):
+            temp_df = pd.read_csv('./reports/' + file_name, sep='/n', header=None, engine='python')
+            temp_df = temp_df[0].str.split(',', expand=True)
+            
+            # Coinbase
+            if 'You' in temp_df.iloc[0][0]:
+                temp_df = temp_df.iloc[3:]
+
+                header_row = 3
+                temp_df.columns = temp_df.iloc[0]
+                temp_df = temp_df.drop(header_row)
+                temp_df = temp_df[['Timestamp', 'Transaction Type', 'Asset', 'Quantity Transacted', 'Spot Price at Transaction', 'Fees']]
+            
+                temp_df = temp_df.rename({'Quantity Transacted': 'Size', 'Spot Price at Transaction': 'Price'}, axis=1)
+                trades = pd.concat([trades, temp_df], ignore_index=True)
+
+            # Coinbase Pro
+            elif 'portfolio' in temp_df.iloc[0][0]:
+                temp_df = temp_df.iloc[0:]
+
+                header_row = 0
+                temp_df.columns = temp_df.iloc[0]
+                temp_df = temp_df.drop(header_row)
+                temp_df = temp_df[['created at', 'side', 'size unit', 'size', 'price', 'fee']]
+            
+                temp_df = temp_df.rename({'created at': 'Timestamp', 'side': 'Transaction Type', 'size unit': 'Asset', 'size':'Size', 'price': 'Price', 'fee': 'Fees'}, axis=1)
+                trades = pd.concat([trades, temp_df], ignore_index=True)
+            
+            # Manual Entry
+            elif 'Timestamp' in temp_df.iloc[0][0]:
+                temp_df = temp_df.iloc[0:]
+
+                header_row = 0
+                temp_df.columns = temp_df.iloc[0]
+                temp_df = temp_df.drop(header_row)
+                if list(temp_df.columns) == ['Timestamp', 'Transaction Type', 'Asset', 'Size', 'Price', 'Fees']:
+                    trades = pd.concat([trades, temp_df], ignore_index=True)
+                else:
+                    print('Manual entry file:' + file_name + 'does not have proper first row.')
+                
+    # Convert numbers to float type
+    trades = trades.fillna(0)
+    trades['Size'] = pd.to_numeric(trades.Size.astype(str).str.replace(',',''), errors='coerce').fillna(0.0).astype(float)
+    trades['Price'] = pd.to_numeric(trades.Price.astype(str).str.replace(',',''), errors='coerce').fillna(0.0).astype(float)
+    trades['Fees'] = pd.to_numeric(trades.Fees.astype(str).str.replace(',',''), errors='coerce').fillna(0.0).astype(float)
+
+    # Save transactions to CSV
+    trades.to_csv('trades.csv', encoding='utf-8', index=False)
+    print(trades)
+
 def load_trades():
     global trades
-    trades = pd.read_csv('trades.csv')
 
     trades['Transaction Type'] = trades['Transaction Type'].str.upper()
     trades['Timestamp'] = pd.to_datetime(trades.Timestamp)
@@ -45,7 +100,7 @@ def get_balance():
 
 def compute_earnings():
     global sell_trades_detailed
-    sell_trades_detailed = pd.DataFrame(columns = ['Timestamp', 'Asset', 'Size', 'Buy Price', 'Sell Price', 'Total Sale', 'Gain', 'Percent Gain'])
+    sell_trades_detailed = pd.DataFrame(columns = ['Asset', 'Size', 'Buy Price', 'Sell Price', 'Total Sale', 'Gain', 'Percent Gain'])
 
     assets = trades['Asset'].unique()
     # filter by coin type
@@ -76,8 +131,14 @@ def compute_earnings():
                     sell_remaining = df_sells.iloc[sell_index]['Size']
                     sell_price = df_sells.iloc[sell_index]['Price']
                 
+                # long term/short term gain (accuracy down to the seconds)
+                seconds_in_day = 24 * 60 * 60
+                seconds_between_trade = (df_sells.iloc[sell_index]['Timestamp']-df_buys.iloc[buy_index]['Timestamp']).days
+                
+                # add trade details
                 sell_trades_detailed = sell_trades_detailed.append({
-                    'Timestamp': df_sells.iloc[sell_index]['Timestamp'],
+                    'Buy Timestamp': df_buys.iloc[buy_index]['Timestamp'],
+                    'Sell Timestamp': df_sells.iloc[sell_index]['Timestamp'],
                     'Asset': asset,
                     'Size': min(buy_remaining, sell_remaining),
                     'Buy Price': buy_price,
@@ -98,7 +159,8 @@ def compute_earnings():
             # calculate any extra using previous cost basis
             if sell_remaining > 0:
                 sell_trades_detailed = sell_trades_detailed.append({
-                    'Timestamp': df_sells.iloc[sell_index]['Timestamp'],
+                    'Buy Timestamp': df_sells.iloc[sell_index]['Timestamp'],
+                    'Sell Timestamp': df_sells.iloc[sell_index]['Timestamp'],
                     'Asset': asset,
                     'Size': min(buy_remaining, sell_remaining),
                     'Buy Price': buy_price,
@@ -108,8 +170,11 @@ def compute_earnings():
                     'Percent Gain': sell_price / buy_price * 100
                 }, ignore_index=True)
 
-    # doesnt work yet
-    sell_trades_detailed['Year'] = pd.DatetimeIndex(sell_trades_detailed['Timestamp']).year
+    sell_trades_detailed['Buy Timestamp'] = pd.DatetimeIndex(sell_trades_detailed['Buy Timestamp'])
+    sell_trades_detailed['Sell Timestamp'] = pd.DatetimeIndex(sell_trades_detailed['Sell Timestamp'])
+    sell_trades_detailed['Days Held'] = (sell_trades_detailed['Sell Timestamp'] - sell_trades_detailed['Buy Timestamp']).astype('timedelta64[D]')
+    sell_trades_detailed['Is Long Term'] = (sell_trades_detailed['Sell Timestamp'] - sell_trades_detailed['Buy Timestamp']).astype('timedelta64[D]') >= 365
+    sell_trades_detailed['Sale Year'] = pd.DatetimeIndex(sell_trades_detailed['Sell Timestamp']).year
     
 
 def print_detailed_report():
@@ -119,7 +184,7 @@ def print_detailed_report():
         return
 
     if FILTER_YEAR:
-        sell_trades_detailed = sell_trades_detailed.loc[sell_trades_detailed['Year'] == YEAR]
+        sell_trades_detailed = sell_trades_detailed.loc[sell_trades_detailed['Sale Year'] == YEAR]
         if len(sell_trades_detailed.index) < 1:
             print('No crypto sales for defined year.')
             return
@@ -138,9 +203,21 @@ def print_detailed_report():
 
     print('Average Observed Gain:')
     print(str(round(sell_trades_detailed['Gain'].sum()/(sell_trades_detailed['Total Sale'].sum())*100, 2)) + '%')
+    print('')
+
+    print('Short Term Earnings:')
+    sell_trades_detailed_short = sell_trades_detailed[sell_trades_detailed['Is Long Term'] == False]
+    print('$' + str(round(sell_trades_detailed_short['Gain'].sum(), 2)))
+    print('')
+
+    print('Long Term Earnings:')
+    sell_trades_detailed_long = sell_trades_detailed[sell_trades_detailed['Is Long Term'] == True]
+    print('$' + str(round(sell_trades_detailed_long['Gain'].sum(), 2)))
+    print('')
 
 
 def main():
+    load_reports()
     load_prices()
     load_trades()
 
